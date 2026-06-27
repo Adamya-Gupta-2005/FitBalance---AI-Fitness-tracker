59429b5d91ea8547ee02edf29b4f556b26288bcc
Adamya Gupta <adamyagupta005@gmail.com>
2026-06-27 16:36:10 +0530

    cookie fixed

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -47,7 +47,7 @@ export const registeredUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-            sameSite: "None",
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 *60 * 1000
         })
 
@@ -108,7 +108,7 @@ export const loginUser = async (req,res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-            sameSite: "None",
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 *60 * 1000
         })
 
